356f0ada4c832f6d768145c7f3018c5a79057371
Ben Allgeier <ballgeier@icloud.com>
2023-08-05 16:36:16 -0500

    Initial Commit

---
+++ UpcomingRocketLaunches/UpcomingRocketLaunchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingRocketLaunchesApp.swift
+//  UpcomingRocketLaunches
+//
+//  Created by Benjamin Allgeier on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct UpcomingRocketLaunchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
